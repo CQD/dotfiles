27ed8a33b77f00200754cc34d1497750b7e4fb56
CQD <wesleyliao@kkbox.com>
2013-06-21 11:25:19 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+BASEDIR=`dirname $0`
+
+#
+umask 077
+
+# get latest
+git pull
+
+#
+cp ${BASEDIR}/.inputrc ~/
+
+#
+#cp ${BASEDIR}/.profile ~/
+cp ${BASEDIR}/.bash_profile ~/
+cp ${BASEDIR}/.bashrc ~/
+
+#
+cp ${BASEDIR}/.tmux.conf ~/
+
+# vim
+cp ${BASEDIR}/.vimrc ~/
+mkdir -p ~/.vim/
+rsync -a ${BASEDIR}/.vim/ ~/.vim/
+chmod 700 ~/.vim/
