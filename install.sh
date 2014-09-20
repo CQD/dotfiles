29aed813df63ea5b34bf360a7598f234c336b2ee
CQD <thecqd@gmail.com>
2014-09-20 10:42:22 +0800

    Add message for install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,24 @@ BASEDIR=`dirname $0`
 umask 077
 
 #
+echo installing bashrc
 #cp ${BASEDIR}/.profile ~/
 cp ${BASEDIR}/.bash_profile ~/
 cp ${BASEDIR}/.bashrc ~/
 
 #
+echo installing tmux config
 cp ${BASEDIR}/.tmux.conf ~/
 
 #
+echo installing git config
 cp ${BASEDIR}/.gitconfig ~/
 
 # vim
+echo installing vim config
 cp ${BASEDIR}/.vimrc ~/
 mkdir -p ~/.vim/
 rsync -a ${BASEDIR}/.vim/ ~/.vim/
 chmod 700 ~/.vim/
+
+echo all done
