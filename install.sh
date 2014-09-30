76996e58ff0889e9ad50d7b514523cc6c35ac86c
CQD <thecqd@gmail.com>
2014-09-30 15:49:47 +0800

    讓 install.sh 照顧 ~/bin/ 裡面的東西

--- install.sh
+++ install.sh
@@ -1,4 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+
+############################################################
+function wg {
+    file_name=$1;
+    remote_url=$2;
+    is_force=$3;
+
+    local_path=~/bin/$file_name
+
+    if [ ! -d ~/bin ]; then
+        mkdir ~/bin
+    fi
+
+    if [ "$is_force" != 'f' ] && [ -f "$local_path" ]; then
+        echo Skipping "$file_name"
+        return;
+    fi
+
+    echo Installing "$file_name" from "$remote_url"
+    wget -q "$remote_url" -O $local_path
+
+    chmod +x $local_path
+}
+
+############################################################
 
 BASEDIR=`dirname $0`
 
@@ -6,25 +31,55 @@ BASEDIR=`dirname $0`
 umask 077
 
 #
-echo installing bashrc
+echo == Installing bashrc
 #cp ${BASEDIR}/.profile ~/
 cp ${BASEDIR}/.bash_profile ~/
 cp ${BASEDIR}/.bashrc ~/
 
 #
-echo installing tmux config
+echo == Installing tmux config
 cp ${BASEDIR}/.tmux.conf ~/
 
 #
-echo installing git config
+echo == Installing git config
 cp ${BASEDIR}/.gitconfig ~/
 cp ${BASEDIR}/.gitignore_global ~/
 
 # vim
-echo installing vim config
+echo == Installing vim config
 cp ${BASEDIR}/.vimrc ~/
 mkdir -p ~/.vim/
 rsync -a ${BASEDIR}/.vim/ ~/.vim/
 chmod 700 ~/.vim/
 
-echo all done
+# ~/bin/
+echo "== Setup ~/bin/"
+PS3='Select operation: '
+options=("Setup" "Override" "Skip" )
+select opt in "${options[@]}"
+do
+  case $opt in
+    "Setup")
+      wg cps https://getcomposer.org/composer.phar
+      wg csf http://get.sensiolabs.org/php-cs-fixer.phar
+      wg psysh http://psysh.org/psysh
+      # todo: phpcs
+      # todo: colordiff
+      break;
+      ;;
+    "Override")
+      wg cps https://getcomposer.org/composer.phar f
+      wg csf http://get.sensiolabs.org/php-cs-fixer.phar f
+      wg psysh http://psysh.org/psysh f
+      # todo: phpcs
+      # todo: colordiff
+      break;
+      ;;
+    *)
+      break;
+      ;;
+  esac
+done
+
+echo
+echo == All done
